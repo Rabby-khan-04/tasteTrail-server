faf0c7f5a0a30d7cb0a23c4216a0ec66f772ea0b
Rabby-khan-04 <ajrabbyk72@gmail.com>
2025-01-25 14:09:00 +0600

    user registration api added

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,6 +1,41 @@
 import { database } from "../db/index.js";
 import ApiError from "../utils/ApiError.js";
+import ApiResponse from "../utils/ApiResponse.js";
 import asyncHandler from "../utils/asyncHandler.js";
+import status from "http-status";
 
-const UserControllers = {};
+const UsersCollection = database.collection("users");
+
+const createUser = asyncHandler(async (req, res) => {
+  const userInfo = req.body;
+
+  if (!(userInfo.email && userInfo.name)) {
+    throw new ApiError(status.NOT_FOUND, "User name and email are required!!");
+  }
+
+  const existedUser = await UsersCollection.findOne({ email: userInfo.email });
+
+  if (existedUser) {
+    throw new ApiError(status.CONFLICT, "User with email already exist!!");
+  }
+  userInfo.role = "user";
+
+  const result = await UsersCollection.insertOne(userInfo);
+
+  const registeredUser = await UsersCollection.findOne({
+    _id: result.insertedId,
+  });
+
+  return res
+    .status(status.CREATED)
+    .json(
+      new ApiResponse(
+        status.OK,
+        registeredUser,
+        "User registered successfully!!"
+      )
+    );
+});
+
+const UserControllers = { createUser };
 export default UserControllers;

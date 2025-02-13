2e8bffd17b1ccfd17d427d73b1cf9c8184e5ecd0
Rabby-khan-04 <ajrabbyk72@gmail.com>
2025-02-14 00:37:13 +0600

    fixed some deploy issue

--- src/app.js
+++ src/app.js
@@ -6,7 +6,12 @@ import cookieParser from "cookie-parser";
 const app = express();
 
 // middlewares
-app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
+app.use(
+  cors({
+    origin: [process.env.CORS_ORIGIN_PROD, process.env.CROS_ORIGIN_LOCAL],
+    credentials: true,
+  })
+);
 app.use(express.json({ limit: "16kb" }));
 app.use(cookieParser());
 app.use(express.urlencoded({ limit: "16kb", extended: true }));
